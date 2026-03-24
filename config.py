044aaa8270f52da13140e0d83fe34030b05e0606
beenomi092-sys <beenomi092@gmail.com>
2026-03-25 00:32:47 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Bot ka token yahan dalo
+BOT_TOKEN = "8622177383:AAEFIIk3OIWitprQLt0a0rTEEp3lGzc40dE"
+
+# Admin IDs (qixing23826)
+ADMIN_IDS = [7681665252]
+
+# Department ka naam
+DEPARTMENT_NAME = "Company HR Department"
+
+# Excel file ka path
+EXCEL_FILE_PATH = "database/users_data.xlsx"
+
+# Images ka folder
+IMAGES_FOLDER = "database/images/"
